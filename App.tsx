e5c3e91662b646796c16e64473b631aeca31c82a
Krollitte <rafael.fernandes@ringa.com.br>
2024-05-04 10:40:44 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>App Contador</Text>
+      <View style={styles.counterArea}>
+        <TouchableOpacity
+          onPress={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          <Text>-</Text>
+        </TouchableOpacity>
+        <Text testID="counter">{counter}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginTop: 14,
+    color: "#000",
+  },
+
+  counterArea: {
+    flexDirection: "row",
+    gap: 14,
+    marginTop: 14,
+    marginBottom: 14,
   },
 });
---
+++ __tests__/app/App.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render } from "@testing-library/react-native";
+import App from "../../App";
+it("example test", () => {
+  expect(1).toBe(1);
+});
+describe("App ComponentTests", () => {
+  it("should render App Component", () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText("App Contador")).toBeTruthy();
+  });
+  it("should change counter when pressing button", () => {
+    const { getByTestId, getByText } = render(<App />);
+    const button = getByText("+");
+    fireEvent.press(button);
+
+    const counterText = getByTestId("counter");
+    expect(counterText.props.children).toBe(1);
+  });
+
+  it("should decrease counter when pressing button", () => {
+    const { getByTestId, getByText } = render(<App />);
+    const button = getByText("-");
+    fireEvent.press(button);
+
+    const counterText = getByTestId("counter");
+    expect(counterText.props.children).toBe(-1);
+  });
+});
